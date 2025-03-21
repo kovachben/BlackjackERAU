ca6f7d70665e002d28468d8d55ef4d2615c060da
sophianoriega <noriegas@my.erau.edu>
2025-03-21 15:55:46 -0700

    Add files via upload

--- playerhand.c
+++ playerhand.c
@@ -10,7 +10,8 @@
 */
 
 
-void totalValueInitialize(int totalValue[], int playerCount) // Resets the initial blackjack value of each hand to zero
+void totalValueInitialize(int totalValue[], int playerCount)
+// Resets the initial blackjack value of each hand to zero
 {
   int i;
   
@@ -44,99 +45,16 @@ void bjValueCalculator(int totalValue[], struct deck Deck[], int playerNumber)
   int i;
   totalValue[playerNumber] = 0; 
   
-  for (i=0; i<52; i++)
+  for (i=51; i>=0; i--)
   {
     if (Deck[i].playerDrawn == playerNumber)
     {
       totalValue[playerNumber] = totalValue[playerNumber] + Deck[i].bjValue;
-    }
-  }
-  
-  if (i<=3 && totalValue[playerNumber] > 21)
-  {
-      totalValue[playerNumber] = totalValue[playerNumber] - 10;
-  }
-}
-
-
-void playOptions(int playerNumber, int totalValue[], struct deck Deck[])
-{
-  int choice;
-  int done = 0;
-  int i;
-  int cardNumber = 1;
-  
-  while (done == 0)
-  {
-    printf("\n\nWould you like to...");
-    printf("\n\n1) Hit\n2) Stand\n3) Double Down\n4) Split");
-    printf("\nEnter your selection: ");
-    scanf("%d", &choice);
-    // Reminder: do errror checking
-    switch (choice)
-    {
-      case 1: // hit
-        drawRandom(Deck, playerNumber); // draws card
-        bjValueCalculator(totalValue, Deck, playerNumber); //Calculates new hand value
-        
-        printf("\nNew Hand:\n\n");
-        for (i=0; i<52; i++)
-        {
-          if (playerNumber == Deck[i].playerDrawn)
-          {
-            printf("Card %d:\n%s of %s\n", cardNumber, Deck[i].card, Deck[i].suit);
-            cardNumber++; 
-          } 
-        }
-        printf("New hand value: %d", totalValue[playerNumber]);
-        cardNumber = 1; // resets card value for next player
-      
-        break; 
-        
-      case 2: // Stand
-        done = 1; 
-        break; 
-      
-      case 3: // Double down function
-        drawRandom(Deck, playerNumber);
-        bjValueCalculator(totalValue, Deck, playerNumber); 
-        done = 1;
-        break; 
-        
-      case 4: 
-        // Split function
-        break; 
-      
-      default: 
-        printf("Please enter a number between 1 and 4");
-        scanf("%d",&choice); // ERROR CHECK
-    } 
-  }
-}
-      
-  
-void playerPrompt(int totalValue[], int playerCount, struct deck Deck[])
-{
-  int i, j;
-  int cardNumber = 1;
-  for (i=0; i<playerCount; i++)
-  {
-    printf("\nDealing player %d's cards!\n", i + 1);
-    sleep(2);
-    
-    for (j=0; j<52; j++)
-    {
-      if (i == Deck[j].playerDrawn)
+      if (i<=3 && totalValue[playerNumber] > 21) // ace logic (EXPLAIN MORE)
       {
-        printf("Card %d:\n%s of %s\n", cardNumber, Deck[j].card, Deck[j].suit);
-        cardNumber++;
+        totalValue[playerNumber] = totalValue[playerNumber] - 10;
       }
     }
-    cardNumber = 1; 
-      
-    printf("\nTotal blackjack value: %d\n", totalValue[i]);
-    
-    playOptions(i, totalValue, Deck);
   }
 }
 
