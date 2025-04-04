a63571aa67c8063083bacfa297eed99430c3923b
kovachben <kovachb1@my.erau.edu>
2025-04-04 16:17:36 -0700

    Add files via upload

--- payout.c
+++ payout.c
@@ -15,45 +15,41 @@ void splitGameEnd(float bets[2][MAX_PLAYERS], int dealerValue, int playerNumber,
   for (i=splitNumber; i<splitNumber+2; i++)
   {
     j++;
-    int end = 0;
-    while (end == 0)
+    if (splitHand[i] > dealerValue && splitHand[i] <= 21 || dealerValue > 21 && splitHand[i] == 21)
     {
-      if (splitHand[i] > dealerValue && splitHand[i] <= 21 || dealerValue > 21 && splitHand[i] == 21)
-      {
-        if (splitHand[i] == 21)
-        {
-          printf("\n\nBlackjack for player %d, in hand %d! You win $%.2f", playerNumber+1, j, (bets[0][playerNumber] / 2) * 1.5);
+     	 if (splitHand[i] == 21)
+       {
+       		printf("\n\nBlackjack for player %d, in hand %d! You win $%.2f", playerNumber+1, j, (bets[0][playerNumber] / 2) * 1.5);
           bets[1][playerNumber] = bets[1][playerNumber] + 1.5 * (bets[0][playerNumber] / 2);
-          continue; 
-        }
-        else
-        {
+          continue;  
+       }
+       else
+       {
           printf("\n\nCongratulations player %d! Hand %d wins $%.2f", playerNumber+1, j, bets[0][playerNumber] / 2);
           bets[1][playerNumber] = bets[1][playerNumber] + bets[0][playerNumber];
           continue;
-        }
-      }
-      if (splitHand[i] == dealerValue)
-      {
-        printf("\n\nPush! Player 2, hand %d's bet of $%.2f is returned.", bets[0][playerNumber] / 2);
-        bets[1][playerNumber] = bets[1][playerNumber] + bets[0][playerNumber] / 2;
-        continue;
-      }  
-      if (dealerValue > 21 && splitHand[i] <= 21)
-      {
-        printf("\n\nCongratulations player %d! Hand %d wins $%.2f", playerNumber+1, j, bets[0][playerNumber] / 2);
-        bets[1][playerNumber] = bets[1][playerNumber] + bets[0][playerNumber];
-        continue;
-      }
-      if (splitHand[i] > 21)
-      {
-        continue;
-      }
-      if (dealerValue > splitHand[i] && dealerValue <= 21)
-      {
-        printf("\n\nPlayer %d's hand %d looses. $%.2f lost.", playerNumber+1, bets[0][playerNumber] / 2);
-        continue;
-      }
+       }
+    }
+    if (splitHand[i] == dealerValue)
+    {
+     	 printf("\n\nPush! Player 2, hand %d's bet of $%.2f is returned.", bets[0][playerNumber] / 2);
+       bets[1][playerNumber] = bets[1][playerNumber] + bets[0][playerNumber] / 2;
+       continue;
+    }  
+    if (dealerValue > 21 && splitHand[i] <= 21)
+    {
+     	 printf("\n\nCongratulations player %d! Hand %d wins $%.2f", playerNumber+1, j, bets[0][playerNumber] / 2);
+       bets[1][playerNumber] = bets[1][playerNumber] + bets[0][playerNumber];
+       continue;
+    }
+    if (splitHand[i] > 21)
+    {
+     	 continue;
+    }
+    if (dealerValue > splitHand[i] && dealerValue <= 21)
+    {
+     	 printf("\n\nPlayer %d's hand %d looses. $%.2f lost.", playerNumber+1, bets[0][playerNumber] / 2);
+       continue;
     }
   }
 }
@@ -87,55 +83,52 @@ void gameEnd(int totalValue[], float bets[2][MAX_PLAYERS], int dealerValue, int 
   int splitNumber = 5;
   for (i=0; i<playerCount; i++)
   {
-    while (end == 0)
-    {
-      if (splitCheck[i] == 0)
+   		if (splitCheck[i] == 0)
       {
-        if (totalValue[i] > dealerValue && totalValue[i] <= 21 || dealerValue > 21 && totalValue[i] == 21)
-        {
-          if (totalValue[i] == 21)
-          {
-            printf("\n\nBlackjack for player %d! You win $%.2f", i+1, bets[0][i] * 1.5);
-            bets[1][i] = bets[1][i] + 1.5 * bets[0][i] + bets[0][i]; // confirm correct
-            continue;
-          }
-          else
-          {
-            printf("\n\nCongratulations player %d! You win $%.2f", i+1, bets[0][i]);
-            bets[1][i] = bets[1][i] + 2 * bets[0][i];
-            continue;
-          }
+       	 if (totalValue[i] > dealerValue && totalValue[i] <= 21 || dealerValue > 21 && totalValue[i] == 21)
+         {
+         		if (totalValue[i] == 21)
+  			 		{
+             	 printf("\n\nBlackjack for player %d! You win $%.2f", i+1, bets[0][i] * 1.5);
+							 bets[1][i] = bets[1][i] + 1.5 * bets[0][i] + bets[0][i]; // confirm correct
+            	 continue;
+	 		 		  }
+          	else
+          	{
+             		printf("\n\nCongratulations player %d! You win $%.2f", i+1, bets[0][i]);
+            		bets[1][i] = bets[1][i] + 2 * bets[0][i];
+            		continue;
+            }
         }
         if (totalValue[i] == dealerValue)
         {
-          printf("\n\nPush! Your bet of $%.2f is returned.", bets[0][i]);
-          bets[1][i] = bets[1][i] + bets[0][i];
-          continue;
+         	 printf("\n\nPush! Your bet of $%.2f is returned.", bets[0][i]);
+           bets[1][i] = bets[1][i] + bets[0][i];
+           continue;
         }
         if (dealerValue > 21 && totalValue[i] <= 21)
         {
-          printf("\n\nCongratulations player %d! You win $%.2f", i+1, bets[0][i]);
-          bets[1][i] = bets[1][i] + 2 * bets[0][i];
-          continue;
+         	 printf("\n\nCongratulations player %d! You win $%.2f", i+1, bets[0][i]);
+           bets[1][i] = bets[1][i] + 2 * bets[0][i];
+           continue;
         }
         if (totalValue[i] > 21)
         {
-          continue;
+           continue;
         }
         if (dealerValue > totalValue[i] && dealerValue <= 21)
         {
-          printf("\n\nPlayer %d looses. $%.2f lost.", i+1, bets[0][i]);
-          continue;
+           printf("\n\nPlayer %d looses. $%.2f lost.", i+1, bets[0][i]);
+           continue;
         }
       }
       else
       {
-        splitGameEnd(bets, dealerValue, i, splitHand, splitCheck, splitNumber);
-        splitNumber += 2; 
-        continue;
+         splitGameEnd(bets, dealerValue, i, splitHand, splitCheck, splitNumber);
+         splitNumber += 2; 
+         continue;
       }
       printReceipt(bets, i, gameCount);
-    }
   }
 }
 
